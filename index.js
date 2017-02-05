4d29b7d5d30d18b72ce83646739f20a41e2a8c05
chocoladisco <chocoladisco@gmail.com>
2017-02-06 00:05:13 +0100

    fixed some codestyle issues I forgot

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-class EloUtil{
-    static expected(rating, ratingopponent){
-        var qa = this._q(rating)
-        var qb = this._q(ratingopponent)
-        return qa / (qa + qb)
-    }
+class EloUtil {
+  static expected (rating, ratingopponent) {
+    var qa = this._q(rating)
+    var qb = this._q(ratingopponent)
+    return qa / (qa + qb)
+  }
 
-    static _q(rating){
-        return Math.pow(10,(rating/400))
-    }
+  static _q (rating) {
+    return Math.pow(10, (rating / 400))
+  }
 
-    static _newRating(rating, expectedrating, actualrating, k){
-        return rating + k * (actualrating - expectedrating)
-    }
+  static _newRating (rating, expectedrating, actualrating, k) {
+    return rating + k * (actualrating - expectedrating)
+  }
 
-    static _calcNewRating(rating, ratingopponent, result){
-        var expected = this.expected(rating, ratingopponent)
-        var k = 64
-        var result = this._newRating(rating, expected, result, k)
-        if (result < 0) {
-            return 0
-        }
-        return result
+  static _calcNewRating (rating, ratingopponent, result) {
+    var expected = this.expected(rating, ratingopponent)
+    var k = 64
+    var rate = this._newRating(rating, expected, result, k)
+    if (rate < 0) {
+      return 0
     }
+    return rate
+  }
 
-    static win(rating, ratingopponent){
-        return this._calcNewRating(rating, ratingopponent, 1)
-    }
+  static win (rating, ratingopponent) {
+    return this._calcNewRating(rating, ratingopponent, 1)
+  }
 
-    static lose(rating, ratingopponent){
-        return this._calcNewRating(rating, ratingopponent, 0)
-    }
+  static lose (rating, ratingopponent) {
+    return this._calcNewRating(rating, ratingopponent, 0)
+  }
 }
 
 module.exports = EloUtil
